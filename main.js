43cbddfe20eaabd0687267a4060505bd929a3f67
Jacqueline Demers <jacquelinedemers@Jacquelines-MacBook-Pro.local>
2018-07-25 14:32:51 -0400

    starting structure of my trivia game

---
+++ main.js
@@ -0,0 +1,19 @@
+
+
+
+//Loop thru all the Questions inside of the array
+
+
+
+
+
+
+//Ask each Question individually
+
+//Get the users response- Then if they are correct or not
+   
+//If their response is correct
+
+
+//Tell them if they were right or wrong
+
